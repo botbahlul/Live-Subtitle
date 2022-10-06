9ec84ac4b66c55cd0541c8cd739ec91996224165
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2022-10-06 20:06:59 +0800

    Delete LANGUAGE_PREFS.java

--- app/src/main/java/com/app/livesubtitle/LANGUAGE_PREFS.java
+++
@@ -1,8 +0,0 @@
-package com.app.livesubtitle;
-
-public class LANGUAGE_PREFS {
-    public static String SRC_DIALECT;
-    public static String SRC;
-    public static String DST_DIALECT;
-    public static String DST;
-}
